92e2bcf6b74d9cd09ec1ec119107ef96450d1a23
cristianh <crishernandez08@hotmail.com>
2023-01-12 15:13:53 -0500

    UPDATE: sirviendo imagenes

--- server.js
+++ server.js
@@ -4,9 +4,22 @@ const router = jsonServer.router('hvdb.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; // <== You can change the port
 
+const express = require('express')
+const app = express();
+const path = require('path')
+app.use('/images', express.static(path.join(__dirname, 'assets')))
+
+
+
+
 server.use(middlewares);
 server.use(router);
 
-server.listen(port,()=>{
+server.listen(port, () => {
     console.log(`servidor corriendo en http://localhost:${port}`);
 });
+
+
+app.listen(3080, () => {
+    console.log(`Example app listening on port:3080`)
+})
