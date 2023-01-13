be5a7f9de3bc60f5cb421feec441777eedf90690
cristianh <crishernandez08@hotmail.com>
2023-01-12 21:14:02 -0500

    UPDATE: quitando opciones de imagenes

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const router = jsonServer.router('hvdb.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; // <== You can change the port
 
-const express = require('express')
-const app = express();
-const path = require('path')
-app.use('/images', express.static(path.join(__dirname, 'assets')))
 
 
 
@@ -19,7 +15,3 @@ server.listen(port, () => {
     console.log(`servidor corriendo en http://localhost:${port}`);
 });
 
-
-app.listen(3080, () => {
-    console.log(`Example app listening on port:3080`)
-})
